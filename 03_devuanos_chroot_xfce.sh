c0e28fc80130e9c8f7e2a7d5e575c926d5a692d8
Bernardino Lopez <bernardino_lopez@yahoo.com>
2019-07-30 00:06:34 -0500

    Added XFCE,LXDE,E17 and OpenBox chroot builders..
    Added XFCE,LXDE,E17 and OpenBox chroot builders..

---
+++ 03_devuanos_chroot_xfce.sh
@@ -0,0 +1,34 @@
+# DevuanOS - Devuan Linux build from scratch with Debootstrap#
+# Bernardino Lopez [ bernardino.lopez@gmail.com ]
+# 7.27.19
+
+# 03_devuanos_chroot.sh - Customize your Distro
+# As root in chroot. Execute the script in a Terminal 
+# ./03_devuanos_chroot.sh
+
+# export LIVE_BOOT=LIVE_BOOT64
+source ./devuanos_config.txt
+
+#echo "devuan-live" > /etc/hostname
+echo $DISTRO_NAME > /etc/hostname 
+
+apt-cache search linux-image
+
+apt-get update && \
+apt-get install -y --no-install-recommends \
+    linux-image-4.9.0-9-amd64 \
+    live-boot
+
+apt-get install -y --no-install-recommends \
+    netbase ifupdown isc-dhcp-client inetutils-ping \
+    network-manager net-tools wireless-tools wpagui \
+    curl openssh-server openssh-client \
+    blackbox xserver-xorg-core xserver-xorg xinit xterm \
+    screenfetch screen lxterminal vim \
+    xfce4* task-xfce-desktop \
+    nano && \
+apt-get clean
+
+passwd root
+
+exit
